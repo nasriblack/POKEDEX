b54925746e560bb472401577c9e67f45c8666755
Nasereddine Lakhal <naserlakhal@gmail.com>
2025-01-25 14:48:30 +0100

    add NoPokemonFound component with styling and navigation; include lucide-react for icons

--- src/pages/NoPokemonFound.tsx
+++ src/pages/NoPokemonFound.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import { ChevronLeft } from "lucide-react";
 
 const NoPokemonFound = () => {
-  return <div>NoPokemonFound</div>;
+  return (
+    <div className="container_nopokemon">
+      <div className="header_nopoekmon">
+        <ChevronLeft
+          width={100}
+          height={100}
+          color="white"
+          cursor={"pointer"}
+          onClick={() => window.history.back()}
+        />
+        <h1 className="title_nopokemon">No Pokemon Found!</h1>
+      </div>
+      <img src="/public/nopokemon_found.png" alt="no_pokemon_found" />
+    </div>
+  );
 };
 
 export default NoPokemonFound;
