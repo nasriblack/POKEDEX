c52624d78e9b2d9bb848518247255be27d836fdf
Nasereddine Lakhal <naserlakhal@gmail.com>
2025-01-25 01:57:32 +0100

    Complete to integrate the search screen

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,5 +1,59 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 const SearchPage = () => {
-  return <div>SearchPage</div>;
+  const [search, setSearch] = useState("");
+  const navigate = useNavigate();
+
+  const handleSearch = async (e: React.FormEvent) => {
+    e.preventDefault();
+    if (!search) return;
+
+    try {
+      navigate(`/pokemon/${search.toLowerCase()}`);
+    } catch (error) {
+      throw new Error(` an Error occurred: ${error}`);
+    }
+  };
+  const handleRandom = async () => {
+    try {
+      navigate(`/pokemon/random`);
+    } catch (error) {
+      throw new Error(` an Error occurred: ${error}`);
+    }
+  };
+  return (
+    <div className="container">
+      <div className="card">
+        <div className="pokeball-container">
+          <img src="/pokeball.png" alt="Pokeball" className="pokeball" />
+        </div>
+        <form onSubmit={handleSearch} className="form">
+          <div className="form-group">
+            <label className="label">Pokemon Name or Id</label>
+            <input
+              type="text"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              className="input"
+            />
+          </div>
+          <div className="button-group">
+            <button type="submit" className="button-primary">
+              Search
+            </button>
+            <button
+              type="button"
+              onClick={handleRandom}
+              className="button-primary"
+            >
+              Random
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default SearchPage;
